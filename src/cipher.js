2fa74fad56b762c7e893a6ae56962ef45d647a55
Sofia <sofiasimas1992@gmail.com>
2020-09-03 10:33:45 -0300

    remove line

--- src/cipher.js
+++ src/cipher.js
@@ -9,7 +9,6 @@ const cipher = {
 
         let result = "";
         const alphabetSize = 26;
-        //for (inicio; teste; incremento)//
         for (let i = 0; i < text.length; i++) {
             let current = text.charCodeAt(i);
             let code = 0;
